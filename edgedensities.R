008ccc5e3ad247def7adb076df68bb093146eabc
Kim Ayers <kayers20@gmail.com>
2025-07-21 14:01:50 -0700

    update edge densities

--- edgedensities.R
+++ edgedensities.R
@@ -12,6 +12,34 @@ edge_densities<-c()
 indivs<-50
 cutoff <- 0.135
 
+#for(k in 1:length(filenames) ){
+ # distancematrix<-read.csv(filenames[k],header=TRUE)
+ # adjacent<-matrix(,nrow=indivs,ncol=indivs)
+ # for (i in 1:indivs){
+#    for (j in 1:indivs){
+ #     if (i==j){
+ #       adjacent[i,j]<-0
+#      }
+  #    else {
+ #       if (distancematrix[i,j] < cutoff) { 
+ #         adjacent[i,j] <- 1
+ #       }
+ #       else {
+ #        adjacent[i,j] <- 0
+ #       }
+ #     }  
+ #   }
+ # }
+
+ # edge_density<-sum(adjacent)/(2*(choose(indivs,2)))
+#  edge_densities[k]<-edge_density
+#}
+
+#df <- data.frame(matrix(unlist(edge_densities), nrow=length(edge_densities), byrow=TRUE))
+
+
+deg_variances<-c()
+
 for(k in 1:length(filenames) ){
   distancematrix<-read.csv(filenames[k],header=TRUE)
   adjacent<-matrix(,nrow=indivs,ncol=indivs)
@@ -25,18 +53,18 @@ for(k in 1:length(filenames) ){
           adjacent[i,j] <- 1
         }
         else {
-         adjacent[i,j] <- 0
+          adjacent[i,j] <- 0
         }
       }  
     }
   }
-
-  edge_density<-sum(adjacent)/(2*(choose(indivs,2)))
-  edge_densities[k]<-edge_density
+  
+  
+  deg_variance<-var(rowSums(adjacent)/2)
+  deg_variances[k]<-deg_variance
 }
 
-df <- data.frame(matrix(unlist(edge_densities), nrow=length(edge_densities), byrow=TRUE))
-
+mean(deg_variances)
 
 
 #g1 <- graph_from_adjacency_matrix(adjacent)
